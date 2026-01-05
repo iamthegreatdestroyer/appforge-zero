f25ba07808c5371f2bbade5414d119007b7c4d90
Steve <sgbilod@gmail.com>
2026-01-04 19:16:36 -0500

    ci: improve ESLint config with TypeScript support and vitest globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,54 @@
 module.exports = {
   root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
   env: {
     node: true,
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
+  plugins: ["@typescript-eslint"],
   rules: {
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
   overrides: [
     {
-      files: ['**/*.ts', '**/*.tsx'],
-      extends: ['eslint:recommended'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+      files: ["**/*.test.ts", "**/*.test.tsx", "**/__tests__/**"],
+      env: {
+        node: true,
+        vitest: true,
       },
-      rules: {
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        jest: "readonly",
+      },
+    },
+    {
+      files: ["**/*.tsx", "src/renderer/**"],
+      env: {
+        browser: true,
+        es2021: true,
+      },
+      globals: {
+        JSX: "readonly",
+        window: "readonly",
+        document: "readonly",
       },
     },
   ],
